5809f8f6f5ba1e9d5f6c513135609392be22da47
SikroYang <yxx95lz@163.com>
2020-12-25 13:37:06 +0800

    抽取方法

--- CoreConsoleApp/Program.cs
+++ CoreConsoleApp/Program.cs
@@ -2,22 +2,31 @@
 
 namespace CoreConsoleApp {
 	class Program {
-		static void Main(string[] args) {
-			bool JIXU = true;
-			do {
-				Console.WriteLine("请输入数字:(按X退出)");
-				var readValue = Console.ReadLine();
-				if (readValue == "X") {
-					JIXU = false;
-				}
-				else {
-					var ceNum = RToCENumber(readValue);
-					Console.WriteLine(ceNum);
-				}
-			} while (JIXU);
-		}
+		static void Main(string[] args)
+        {
+            x_Exit();
+        }
 
-		private static string RToCENumber(string readValue) {
+        private static void x_Exit()
+        {
+            bool JIXU = true;
+            do
+            {
+                Console.WriteLine("请输入数字:(按X退出)");
+                var readValue = Console.ReadLine();
+                if (readValue == "X")
+                {
+                    JIXU = false;
+                }
+                else
+                {
+                    var ceNum = RToCENumber(readValue);
+                    Console.WriteLine(ceNum);
+                }
+            } while (JIXU);
+        }
+
+        private static string RToCENumber(string readValue) {
 			try {
 				var readNum = Convert.ToInt32(readValue) - 5474;
 				var num = Convert.ToInt16("FFFF", 16) + Convert.ToInt16("1", 16);
